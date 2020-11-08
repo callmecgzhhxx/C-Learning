7f9e2e53ea611ea8561a7260ffd249b9821d4399
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2020-11-08 23:01:23 +0800

    hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: YoYoLikesCiCi-孙起
+	> Mail: YoYoLikesCiCi@live.cn 
+	> Created Time: Sun Nov  8 22:59:57 2020
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hello cgz, 这是我们共同的仓库，我们俩可以同时编辑这个仓库并推送，对方可以同时查看");
+	getchar();
+	return 0;
+}
