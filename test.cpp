52100e63098fadcd85a1a6b142559d80b333f18c
陈光泽 <>
2020-11-10 17:27:41 +0800

    分段函数 在vs2010里要把int改为double才可以运行

--- test.cpp
+++ test.cpp
@@ -1,7 +1,24 @@
+//分段函数
+#include<math.h>
 #include<iostream>
-int main(){
-	cout <<"我已经在我的电脑上进行更改了";
-	cout<<"hello!"<<endl;
-	cout<<"you are so ugly"<<endl;
-	return 0;
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"shuru x= ";
+    cin>>x;
+    cout<<endl;
+    if(x<1)
+    cout<<"y= "<<x<<endl;
+
+    if(x>=1&&x<5)
+    cout<<"y= "<<2*x-1<<endl;
+
+    if(x>=5&&x<10)
+    cout<<"y= "<<3*pow(x,2)-11<<endl;
+
+    if(x>=10)
+    cout<<"y= "<<4*pow(x,3)-3*pow(x,2)-21<<endl;
+
+    return 0;
 }
